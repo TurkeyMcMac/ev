95757d737fc5365dd776b121554af6968985708e
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-10-08 21:33:52 -0400

    Implement all brain mutation functions

--- brain.c
+++ brain.c
@@ -44,7 +44,16 @@ char* Layer_compute(struct Layer* self, char* input) {
 	return output;
 }
 
-struct Layer Layer_mutate(struct Layer* self, float amount);
+struct Layer Layer_mutate(struct Layer* self, float amount) {
+	struct Layer l;
+	l.neurons = malloc(self->neuron_num * sizeof(struct Neuron));
+	for (size_t i = 0; i < self->neuron_num; ++i)
+		l.neurons[i] = Neuron_mutate(self->neurons + i, amount);
+
+	l.neuron_num = self->neuron_num;
+
+	return l;
+}
 
 struct Layer Layer_random(float amount, size_t neuron_num, size_t weight_num) {
 	struct Layer l;
@@ -74,7 +83,16 @@ char* Brain_compute(struct Brain* self, char* input) {
 	return last_output;
 }
 
-struct Brain Brain_mutate(struct Brain* self, float amount);
+struct Brain Brain_mutate(struct Brain* self, float amount) {
+	struct Brain b;
+	b.layers = malloc(self->layer_num * sizeof(struct Layer));
+	for (size_t i = 0; i < self->layer_num; ++i)
+		b.layers[i] = Layer_mutate(self->layers + i, amount);
+
+	b.layer_num = self->layer_num;
+
+	return b;
+}
 
 struct Brain Brain_random(float amount, size_t input_num, size_t* layers, size_t layer_num) {
 	struct Brain b;
@@ -99,8 +117,9 @@ int main() {
 	srand(time(NULL));
 	
 	size_t layers[3] = {5, 6, 5};
-	
-	struct Brain b = Brain_random(1.0, 5, (size_t*)&layers, 3);
+
+	struct Brain b_before = Brain_random(1.0, 5, (size_t*)&layers, 3);	
+	struct Brain b = Brain_mutate(&b_before, 1.0);
 	
 	char input[5] = {'\0', '\1', '\0', '\1', '\0'};
 
