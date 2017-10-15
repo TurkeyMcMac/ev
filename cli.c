eba822916d0b637352ad3a6e7b99a44bd1ef1539
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-10-15 19:15:09 -0400

    Move argument parsing to a separate function

--- cli.c
+++ cli.c
@@ -63,91 +63,98 @@ ssize_t parse_integer_list(char* src, size_t* dst, size_t cap) {
 	return l;
 }
 
+int parse_arg(struct ProgConfig* conf, char* arg) {
+	switch (*arg) {
+		case 'W': // width
+			conf->world_width = (size_t)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'H': // height
+			conf->world_height = (size_t)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'o': // organism generation chance
+			conf->seed.organism = (unsigned char)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'f': // food generation chance
+			conf->seed.food = (unsigned char)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'r': // rock generation chance
+			conf->seed.rock = (unsigned char)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'L': // nn layers
+			conf->world.nn_layers = malloc(INITIAL_INT_LIST_SIZE * sizeof(size_t));
+
+			ssize_t layer_num = parse_integer_list(arg, conf->world.nn_layers, INITIAL_INT_LIST_SIZE);
+
+			if (layer_num >= 0)
+				conf->world.nn_layer_num = layer_num;
+			else return OUT_OF_MEMORY;
+
+			break;
+		case 'n': // food nutrition
+			conf->world.nutrition = (unsigned int)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'F': // start fullness
+			conf->world.fullness = (unsigned int)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 't': // max start fullness threshold
+			conf->world.fullness_threshold_max = (unsigned int)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'm': // start mutation
+			conf->world.start_mutation = strtof(&arg[1], NULL);
+
+			break;
+		case 'M': // mutation
+			conf->world.mutation = strtof(&arg[1], NULL);
+
+			break;
+		case 'c': // mutation chance
+			conf->world.mutation_chance = (unsigned int)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'p': // minimum population
+			conf->minimum_population = (size_t)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'i': // number of world cycles between frames
+			conf->ticks_per_frame = (unsigned long)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'S': // seconds per frame
+			conf->frame_delay.tv_sec = (time_t)strtol(&arg[1], NULL, 10);
+
+			break;
+		case 's': // nanoseconds per frame
+			conf->frame_delay.tv_nsec = strtol(&arg[1], NULL, 10);
+
+			break;
+		case 'h': // help
+		case '?':
+			fprintf(stderr, "%s\n", HELP_INFO);
+
+			return SHOWED_HELP;
+		default:
+			fprintf(stderr, "Invalid argument: '%c'\n%s\n", *arg, HELP_INFO);
+
+			return INVALID_ARGUMENT;
+	}
+
+	return 0;
+}
+
 int load_config_to(struct ProgConfig* conf, int argc, char** argv) {
 	for (int i = 1; i < argc; ++i) {
 		char* arg = argv[i];
 
-		switch (arg[1]) {
-			case 'W': // width
-				conf->world_width = (size_t)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'H': // height
-				conf->world_height = (size_t)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'o': // organism generation chance
-				conf->seed.organism = (unsigned char)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'f': // food generation chance
-				conf->seed.food = (unsigned char)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'r': // rock generation chance
-				conf->seed.rock = (unsigned char)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'L': // nn layers
-				conf->world.nn_layers = malloc(INITIAL_INT_LIST_SIZE * sizeof(size_t));
-
-				ssize_t layer_num = parse_integer_list(&arg[1], conf->world.nn_layers, INITIAL_INT_LIST_SIZE);
-
-				if (layer_num >= 0)
-					conf->world.nn_layer_num = layer_num;
-				else return OUT_OF_MEMORY;
-
-				break;
-			case 'n': // food nutrition
-				conf->world.nutrition = (unsigned int)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'F': // start fullness
-				conf->world.fullness = (unsigned int)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 't': // max start fullness threshold
-				conf->world.fullness_threshold_max = (unsigned int)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'm': // start mutation
-				conf->world.start_mutation = strtof(&arg[2], NULL);
-
-				break;
-			case 'M': // mutation
-				conf->world.mutation = strtof(&arg[2], NULL);
-
-				break;
-			case 'c': // mutation chance
-				conf->world.mutation_chance = (unsigned int)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'p': // minimum population
-				conf->minimum_population = (size_t)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'i': // number of world cycles between frames
-				conf->ticks_per_frame = (unsigned long)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'S': // seconds per frame
-				conf->frame_delay.tv_sec = (time_t)strtol(&arg[2], NULL, 10);
-
-				break;
-			case 's': // nanoseconds per frame
-				conf->frame_delay.tv_nsec = strtol(&arg[2], NULL, 10);
-
-				break;
-			case 'h': // help
-			case '?':
-				fprintf(stderr, "%s\n", HELP_INFO);
-
-				return SHOWED_HELP;
-			default:
-				fprintf(stderr, "Invalid argument: '%c'\n%s\n", arg[1], HELP_INFO);
-
-				return INVALID_ARGUMENT;
-		}
+		int err = parse_arg(conf, &arg[1]);
+		if (err) return err;
 	}
 
 	return 0;
